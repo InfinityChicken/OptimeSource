d1336e8087fe0ba8e8a506caf3caa718679d6c5b
SunGame7 <88058857+hypixel-js@users.noreply.github.com>
2022-12-06 17:59:15 -0800

    added comments

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const express = require("express")
-var app = express()
+const express = require("express") /* Imports express (the server module) */
+var app = express() /* Makes a new server */
 
+/** app.get is run when a path is loaded on a browser */
 app.get('/', async (req, res) => {
-    res.redirect('/web/index.html')
+    res.redirect('/web/index.html') /* redirects to the index page if there's no additional path */
 })
 
 app.get('/web/:name', async (req, res) => {
-    res.sendFile(__dirname + '/main/' + req.params.name)
+    res.sendFile(__dirname + '/main/' + req.params.name) /* example.com/web/file will load main/file */
 })
 
-app.listen(80, () => {})
+
+
+app.get('/accept', async (req, res) => {
+
+})
+
+app.listen(80, () => {}) /* Boots the server */
