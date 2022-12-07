fb6774674be8f124e1e67ac3fd72475134e570cc
SunGame7 <88058857+hypixel-js@users.noreply.github.com>
2022-12-06 16:59:27 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require("express")
 var app = express()
 
+app.get('/', async (req, res) => {
+    res.redirect('/web/index.html')
+})
+
+app.get('/web/:name', async (req, res) => {
+    res.sendFile(__dirname + '/main/' + req.params.name)
+})
+
 app.listen(80, () => {})
