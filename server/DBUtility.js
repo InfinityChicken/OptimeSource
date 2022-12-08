e9d6cdb4f48e1ae04f05f2686604535e29023dbb
InfinityChicken <8zsc2014@gmail.com>
2022-12-07 17:41:48 -0800

    sorry
    this might break things

--- server/DBUtility.js
+++ server/DBUtility.js
@@ -19,28 +19,44 @@ var database = mysql.createConnection({ //to use this, you must whitelist your i
   port: process.env.PORT,
 });
 
+<<<<<<< Updated upstream
 function createEventObject(tag) {
   let event = {
     tag: tag, //TODO: get tag from html
     name: database.query(), //TODO: add parameters
     description: database.query(),
     startTime: database.query(),
+=======
+function eventObject(tag) {
+  return {
+    tag: tag, //string
+    name: database.query(), //string
+    description: database.query(), //string
+    startTime: database.query(), 
+>>>>>>> Stashed changes
     endTime: database.query(),
     duration: database.query(),
     draft: database.query(), //will return 0 or 1 -- 0 = false, 1 = true
+    progress: database.query(), //will return in-progress, finished, or upcoming
     //color: database.query(),
   }
   return event;
 }
 
+<<<<<<< Updated upstream
 function createUserObject(tag) {
   let user = {
+=======
+function userObject(tag) {
+  return {
+>>>>>>> Stashed changes
     tag: tag, //TODO: get tag from html
     username: database.query(), //TODO: add parameters
     password: database.query(),
     email: database.query(),
     firstName: database.query(),
     lastName: database.query(),
+<<<<<<< Updated upstream
   //status: database.query(),
   }
   return user;
@@ -52,3 +68,52 @@ function createRSVPObject(eventTag) {
     users: (database.query()).split("")
   }
 }
+=======
+    //status: database.query(),
+  };
+}
+
+function userRSVP(userTag, eventTag) {
+  return {
+    rsvpStatus: database.query(),
+  }
+}
+
+function userEvents(userTag) {
+  let events = [];
+  while (true) {
+    const currentEvent = database.query();
+    events += currentEvent;
+    if (database.query() = null) {
+      break;
+    } 
+  }
+  return {
+    events,
+    rsvpStatus: database.query(),
+  };
+}
+
+function eventInvitees(eventTag) {  
+  return {
+    users: database.query(),
+    rsvpStatuses: database.query(),
+  };
+}
+
+function writeToEvents() {
+  return null;
+}
+
+function writeToUsers() {
+  return null;
+}
+
+function writeToRSVPs() {
+  return null;
+}
+
+module.exports.eventObject = eventObject;
+module.exports.userObject = userObject;
+module.exports.database = database;
+>>>>>>> Stashed changes
--- server/EventCalculator.js
+++ server/EventCalculator.js
@@ -2,8 +2,41 @@ import {createEventObject} from DBUtility.js;
 import {createUserObject} from DBUtility.js;
 
 
+<<<<<<< Updated upstream
 function findOptimalTime(eventTag) {
     const event = createEventObject(tag);
 }
 
 function findOverlap(eventTag, event, user)
+=======
+function passValuesCalculator() { //TODO: fill this up later and pass to findOptimalTime
+    return null; //TODO: get magical millisecond values from here using .getTime();
+}
+
+function findOptimalTime(possibleTimes, duration, invitedUsers) {  //possibleTimes: array of arrays, array1 stores possible times, array2 stores start time, end time, date
+    for (invitedUsers)
+    
+    let optimalTimes = [[]]; //array1 stores optimal times, array2 stores starttime, endtime, and errorate
+    for (let perI = 0; perI < possibleTimes.length(); perI++) { //loops over all events
+        for (let trialEnd = possibleTimes[perI[0]] + duration; trialEnd <= possibleTimes[perI[1]]; trialEnd+=300000) { //iterations of 5 minutes (300,000 miliseconds)
+            trialStart = trialEnd - duration;
+            errorRate = findOverlap(trialStart, trialEnd, invitedUsers);
+            if (errorRate = optimalTimes[0[2]]) {
+                optimalTimes.push([trialStart, trialEnd, trial]); //TODO: this might break something so check it when testing             
+            }
+        }
+    }
+}
+
+
+function findOverlap(trialStart, trialEnd, invitedUsers) {
+    for (let userI = 0; userI < invitedUsers.length; userI++) {
+        for (let userI = 0; userI < events.length(); userI++) {
+            const userEvents = DBUtility.userEvents(invitedUsers[userI]);
+            userevents[1]
+        }
+    }
+}
+
+module.exports.passValuesCalculator = passValuesCalculator;
+>>>>>>> Stashed changes
