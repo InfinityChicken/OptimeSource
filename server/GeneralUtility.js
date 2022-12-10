cd50800fb565e744432011670e020d6c88a15074
SunGame7 <neil.thella@gmail.com>
2022-12-09 20:16:24 -0800

    Update GeneralUtility.js

--- server/GeneralUtility.js
+++ server/GeneralUtility.js
@@ -1,13 +1,15 @@
-function generateTag(name) {
-    let tag = name + "#";
-    for (i=1; i<=4; i++) {
-        let random = (Math.floor(Math.random() * 10)).toString();
-        tag += random;
-    }   
-
-    if (tag = null) { //TODO: add an sql query here to check if a tag preexists
-        generateTag(name);
+module.exports = {
+    generateTag: function(name) {
+        let tag = name + "#";
+        for (i=1; i<=4; i++) {
+            let random = (Math.floor(Math.random() * 10)).toString();
+            tag += random;
+        }   
+    
+        if (tag = null) { //TODO: add an sql query here to check if a tag preexists
+            generateTag(name);
+        }
+    
+        return tag;
     }
-
-    return tag;
 }
