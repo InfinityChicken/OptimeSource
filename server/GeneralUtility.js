be88ae5d2d925f91929a26228ef5efe24c33f673
SunGame7 <neil.thella@gmail.com>
2022-12-09 20:18:47 -0800

    minor functionality changes

--- server/GeneralUtility.js
+++ server/GeneralUtility.js
@@ -4,9 +4,9 @@ module.exports = {
         for (i=1; i<=4; i++) {
             let random = (Math.floor(Math.random() * 10)).toString();
             tag += random;
-        }   
+        }
     
-        if (tag == null) { //TODO: add an sql query here to check if a tag preexists
+        if (!tag) { //TODO: add an sql query here to check if a tag preexists
             generateTag(name);
         }
         return tag;
