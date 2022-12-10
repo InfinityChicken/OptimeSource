37c0b48a576a842c33dda669619bd59ab82f58dd
SunGame7 <neil.thella@gmail.com>
2022-12-09 20:13:22 -0800

    Update DBUtility.js

--- server/DBUtility.js
+++ server/DBUtility.js
@@ -99,7 +99,7 @@ function addEventToDB(event) {
     var sql = "INSERT INTO events (name, description, startTime, endTime, eventType) VALUES ('" + event.name + "', '" + event.description + "','" + event.startTime + "','" + event.endTime + "','" + event.eventType + "')";
   database.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
+    console.log("event created");
   });
   }
 
