64c037e4da56681ff66c36816bfa8512c09c7ec3
SunGame7 <neil.thella@gmail.com>
2022-12-09 20:26:06 -0800

    Update GeneralUtility.js

--- server/GeneralUtility.js
+++ server/GeneralUtility.js
@@ -2,7 +2,7 @@ module.exports = {
     baseGenerate: function(name) {
         var tag = name + "#";
         for (i=0; i<4; i++) tag += Math.floor(Math.random() * 10)
-        if (!tag) tag = baseGenerate(name) //TODO: add an sql query here to check if a tag preexists
+        if (!tag /* replace with sql condition */ ) tag = baseGenerate(name) //TODO: add an sql query here to check if a tag preexists
         return tag;
     },
     generateTag: function(name) { return this.baseGenerate(name) }
