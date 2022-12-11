e08e74ec9cd27777783f7747e7bfb306fdd12dfd
SunGame7 <neil.thella@gmail.com>
2022-12-11 14:59:18 -0800

    fixed eventObject() & userObject()

--- server/DBUtility.js
+++ server/DBUtility.js
@@ -1,57 +1,74 @@
 var mysql = require("mysql");
 require('dotenv').config()
+require("dotenv").config();
 
-var database = mysql.createConnection({ //to use this, you must whitelist your ip address in digitalocean 
+console.log({
+  //to use this, you must whitelist your ip address in digitalocean
   host: process.env.HOST,
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   port: process.env.PORT,
-  database: 'defaultdb'
+  database: "defaultdb",
 });
 
-function eventObject(tag) {
-  let event = {
-      tag: tag,
-      name: "",
-      description: "",
-      startTime: "",
-      endTime: "",
-  }
+var database = mysql.createConnection({
+  //to use this, you must whitelist your ip address in digitalocean
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+  database: "defaultdb",
+});
 
-  database.query("SELECT * FROM events WHERE eventID=", function (err, result, fields) { //TODO: make this work
-    if (err) throw err;
-      result.forEach((row) => {
-      event.name = row.name;
-      event.description = row.description;
-      event.startTime = row.startTime;
-      event.endTime = row.endTime;
-      event.eventType = row.eventType;
-    });
+async function lol() {
+  console.log(await userObject("IRIS"));
+}
+lol();
+
+function eventObject(tag) {
+  return new Promise((resolve, reject) => {
+    let event = { tag: tag };
+    database.query(
+      `SELECT * FROM events WHERE eventID='${tag}'`,
+      function (err, result, fields) {
+        //TODO: make this work
+        if (err) throw err;
+        if (result.length == 0) return resolve(null);
+        row = result[0];
+        event.name = row.name;
+        event.description = row.description;
+        event.startTime = row.startTime;
+        event.endTime = row.endTime;
+        event.eventType = row.eventType;
+        resolve(event);
+      }
+    );
   });
-  return event;
 }
 
 async function userObject(username) {
   return new Promise((resolve, reject) => {
-    user = {}
-    database.query(`SELECT * FROM user_info WHERE id='${username}'`, function (err, result, fields) {
-      if (err) throw err;
-      if (result.length == 0) return(resolve(null))
+    user = {};
+    database.query(
+      `SELECT * FROM user_info WHERE id='${username}'`,
+      function (err, result, fields) {
+        if (err) throw err;
+        if (result.length == 0) return resolve(null);
         result.forEach((row) => {
-        // console.log(row.description);
+          // console.log(row.description);
           user = {
             id: row.id,
-            email: row.email,
             password: row.password,
-            tag: row.tag
-          }
-        resolve(user);
-      });
-    });
-  })
+          };
+          resolve(user);
+        });
+      }
+    );
+  });
 }
 
-function userEvents(userTag, status) { //return the events that a user is a part of -- scan RSVPs; if there is a status specific (status that isn't null: yes, no, maybe, or pending), return only the RSVPs that are said status; modify the database if necessary
+function userEvents(userTag, status) {
+  //return the events that a user is a part of -- scan RSVPs; if there is a status specific (status that isn't null: yes, no, maybe, or pending), return only the RSVPs that are said status; modify the database if necessary
   return {
     events: database.query(`SELECT ${userTag}, eventID FROM RSVPs`),
     rsvpStatus: database.query(`SELECT ${userTag}`),
@@ -59,26 +76,47 @@ function userEvents(userTag, status) { //return the events that a user is a part
 }
 
 function addEvent(event) {
-  var sql = "INSERT INTO events (name, description, startTime, endTime, eventType) VALUES ('" + event.name + "', '" + event.description + "','" + event.startTime + "','" + event.endTime + "','" + event.eventType + "')";
-database.query(sql, function (err, result) {
-  if (err) throw err;
-});
+  var sql =
+    "INSERT INTO events (name, description, startTime, endTime, eventType) VALUES ('" +
+    event.name +
+    "', '" +
+    event.description +
+    "','" +
+    event.startTime +
+    "','" +
+    event.endTime +
+    "','" +
+    event.eventType +
+    "')";
+  database.query(sql, function (err, result) {
+    if (err) throw err;
+  });
 }
 
 function addUser(user) {
-  var sql = "INSERT INTO events (id, password) VALUES ('" + user.username + "', '" + user.pasword + "')";
-database.query(sql, function (err, result) {
-  if (err) throw err;
-});
+  var sql =
+    "INSERT INTO events (id, password) VALUES ('" +
+    user.username +
+    "', '" +
+    user.pasword +
+    "')";
+  database.query(sql, function (err, result) {
+    if (err) throw err;
+  });
 }
 
-function modifyEvent (eventID, field, newValue){
-
-}
+function modifyEvent(eventID, field, newValue) {}
 
 //delete things?
 
-module.exports = {eventObject, userObject, userEvents, rsvpStatus, addEvent, addUser}
+module.exports = {
+  eventObject,
+  userObject,
+  userEvents,
+  // rsvpStatus,
+  addEvent,
+  addUser,
+};
 
 // module.exports.eventObject = eventObject;
 // module.exports.userObject = userObject;
--- server/EventCalculator.js
+++ server/EventCalculator.js
@@ -1,3 +1,5 @@
+//TODO: export calculator, connect the calculator
+
 /* how the code works:
 -makes some variables
 -loops through possible times
@@ -6,59 +8,72 @@
             -loops through users' events
 */
 
+var DBUtility = require(__dirname + "/DBUtility.js");
 
-var DBUtility = require(__dirname + '/DBUtility.js');
+function findOptimalTime(possibleTimes, invitedUsers, duration) {
+  //possibleTimes: array of arrays, array1 stores possible times, array2 stores start time, end time
 
-function findOptimalTime(possibleTimes, invitedUsers, duration) {  //possibleTimes: array of arrays, array1 stores possible times, array2 stores start time, end time
+  let optimalTimes = [[null, null, Number.MAX_VALUE]]; //array1 stores optimal times, array2 stores starttime, endtime, and errorate
 
-    let optimalTimes = [[null, null, Number.MAX_VALUE]]; //array1 stores optimal times, array2 stores starttime, endtime, and errorate
+  for (let perI = 0; perI < possibleTimes.length; perI++) {
+    //loops over all trial times
+    let trialI = 0;
+    while (true) {
+      let trialStart = possibleTimes[perI][0] + trialI * 300;
 
-    for (let perI = 0; perI < possibleTimes.length; perI++) { //loops over all trial times
-        let trialI = 0;
-        while (true) {
-            let trialStart = possibleTimes[perI][0] + (trialI*300);
-            
-            let trialEnd = trialStart + duration;
-            
-            if (trialEnd > possibleTimes[perI][1]) {
-                break;
-            }
-            
-            errorRate = findOverlap(trialStart, trialEnd, invitedUsers);
-            
-            if (errorRate == optimalTimes[0][2]) { //if error rate is equivalent, add the times
-                optimalTimes.push([trialStart, trialEnd, errorRate]);    
-            } else if (errorRate < optimalTimes[0][2]) { //if error rate is lower, clear the array and add the times
-                optimalTimes = [];
-                optimalTimes.push([trialStart, trialEnd, errorRate]);
-            }
-            
-            trialI++;
-        }
+      let trialEnd = trialStart + duration;
+
+      if (trialEnd > possibleTimes[perI][1]) {
+        break;
+      }
+
+      errorRate = findOverlap(trialStart, trialEnd, invitedUsers);
+
+      if (errorRate == optimalTimes[0][2]) {
+        //if error rate is equivalent, add the times
+        optimalTimes.push([trialStart, trialEnd, errorRate]);
+      } else if (errorRate < optimalTimes[0][2]) {
+        //if error rate is lower, clear the array and add the times
+        optimalTimes = [];
+        optimalTimes.push([trialStart, trialEnd, errorRate]);
+      }
+
+      trialI++;
     }
+  }
 
-    return optimalTimes;
+  return optimalTimes;
 }
 
 function findOverlap(trialStart, trialEnd, invitedUsers) {
-    let errorRate = 0;
-    for (let userI = 0; userI < invitedUsers.length; userI++) { //iterates through users
-        const userEvents = DBUtility.userEvents(invitedUsers[userI]); 
+  let errorRate = 0;
+  for (let userI = 0; userI < invitedUsers.length; userI++) {
+    //iterates through users
+    const userEvents = DBUtility.userEvents(invitedUsers[userI]);
 
-        for (let eventI = 0; eventI < userEvents.length; eventI++) { //iterates through events
-            const event = DBUtility.eventObject(userEvents[eventI]);
+    for (let eventI = 0; eventI < userEvents.length; eventI++) {
+      //iterates through events
+      const event = DBUtility.eventObject(userEvents[eventI]);
 
-            if (event.startTime < trialStart && trialStart < event.endTime || event.startTime < trialEnd && trialEnd < event.endTime) { //if the trial time starts/ends during the tested event
-                errorRate++;
-            } else if (trialStart < event.startTime && event.startTime < trialEnd || trialStart < event.endTime && event.endTime < trialEnd) { //if the event lies in the trial period 
-                errorRate++; 
-            } else if (trialStart == event.startTime || trialEnd == event.endTime) { //if the event is exactly the same time
-                errorRate++;
-            } //if none of those things are found, advance to the next event without increasing the error rate
-        }
+      if (
+        (event.startTime < trialStart && trialStart < event.endTime) ||
+        (event.startTime < trialEnd && trialEnd < event.endTime)
+      ) {
+        //if the trial time starts/ends during the tested event
+        errorRate++;
+      } else if (
+        (trialStart < event.startTime && event.startTime < trialEnd) ||
+        (trialStart < event.endTime && event.endTime < trialEnd)
+      ) {
+        //if the event lies in the trial period
+        errorRate++;
+      } else if (trialStart == event.startTime || trialEnd == event.endTime) {
+        //if the event is exactly the same time
+        errorRate++;
+      } //if none of those things are found, advance to the next event without increasing the error rate
     }
-    return errorRate;
+  }
+  return errorRate;
 }
 
 module.exports.findOptimalTime = findOptimalTime;
-module.exports.findOverlap = findOverlap;
