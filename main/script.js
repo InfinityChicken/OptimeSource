1ead0e61192d7de0b72a65b87d5ffd962f52a73c
advita-g <110643458+advita-g@users.noreply.github.com>
2022-12-11 18:58:13 -0800

    final ?

--- main/script.js
+++ main/script.js
@@ -25,33 +25,33 @@ if (urlParams.get('setemail')) document.cookie = `email=${urlParams.get('setemai
 
 var rsvps = [
     {
-        id: 1,
-        name: "Park Meetup",
+        id: 2,
+        name: "Hackathon meeting",
         start: 1670194800000,
         end: 1670205600000,
         localStatus: 0, // 0: No, 1: Maybe, 2: Yes, null: Unanswered
-        description: "A fun park meetup! OwO!",
-        host: "Goyal, Adveeta"
+        description: "Finish UI",
+        host: "Cheng, Zachary"
     },
     {
-        id: 2,
-        name: "Poger Momento",
-        start: 1670204800000,
-        end: 1670205680000,
-        localStatus: 0, // 0: No, 1: Maybe, 2: Yes, null: Unanswered
-        description: "A poger momento ngl.",
-        host: "Ding, Amber Wanyi"
+        id: 1,
+        name: "Potluck!",
+        start: 1670194800000,
+        end: 1670205600000,
+        localStatus: 2, // 0: No, 1: Maybe, 2: Yes, null: Unanswered. Only display events with 1 or 2
+        description: "Don't bring anything including nuts!",
+        host: "Goyal, Advita"
     }
 ]
 var upcoming = [
     {
         id: 1,
-        name: "Park Meetup",
+        name: "Potluck!",
         start: 1670194800000,
         end: 1670205600000,
         localStatus: 2, // 0: No, 1: Maybe, 2: Yes, null: Unanswered. Only display events with 1 or 2
-        description: "A fun park meetup! OwO!",
-        host: "Goyal, Adveeta"
+        description: "Don't bring anything including nuts!",
+        host: "Goyal, Advita"
     }
 ]
 
@@ -91,8 +91,11 @@ if (upcoming.length == 0) {
     event1.innerHTML=`${upcoming[0].name}<br><p style="font-size: 20px;">${date} ${startTime}-${endTime}<br>${upcoming[0].description}<br>Host: ${upcoming[0].host}</p>`
 }
 if (upcoming.length < 5) {
-    document.querySelector('#event5').innerHTML = ":("
-    if (upcoming.length < 4) document.querySelector('#event4').innerHTML = ":("
-    if (upcoming.length < 3) document.querySelector('#event3').innerHTML = ":("
-    if (upcoming.length < 2) document.querySelector('#event2').innerHTML = ":("
-}
+    document.querySelector('#event5').innerHTML = ""
+    if (upcoming.length < 4) document.querySelector('#event6').innerHTML = "Going to the movies <br> 1/25/2022 9:30-11:00 <br> Host: Li, Mooncy"
+    if (upcoming.length < 4) document.querySelector('#event5').innerHTML = "Shopping Spree <br> 1/20/2022 7:00-9:30 <br> Host: Ding, Jasper"
+    if (upcoming.length < 4) document.querySelector('#event4').innerHTML = "Project Meetup <br> 1/15/2022 9:00-11:00 <br> Host: Parachuri, Sid"
+    if (upcoming.length < 3) document.querySelector('#event3').innerHTML = "New Year's Dinner Party <br> 1/1/2022 6:00-8:30 <br> Host: Chen, Iris"
+    if (upcoming.length < 2) document.querySelector('#event2').innerHTML = "Neil's Birthday Party! <br> 12/23/2022 12:00-3:00 <br> Host: Thella, Neil"
+    if (upcoming.length < 2) document.querySelector('#event1').innerHTML = "Park Meetup <br> 12/12/2022 4:00-7:00 <br> A fun park meetup! <br> Host: Goyal, Advita"
+ }
